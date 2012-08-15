bbf2117c9576fae695705e6f2d3b8658dcb977c3
Blabos de Blebe <blabos@blabos.org>
2012-08-15 03:01:44 -0300

    Add Paginator::Lite. close #9

--- lib/Poste/Controller/Posts.pm
+++ lib/Poste/Controller/Posts.pm
@@ -2,20 +2,41 @@ package Poste::Controller::Posts;
 
 use Mojo::Base 'Mojolicious::Controller';
 
+use Paginator::Lite;
 
 sub startup {
     my ( $self, $c ) = @_;
 
     my $r = $c->routes;
 
-    $r->route('/posts')->to('posts#index');
+    $r->route('/posts/')->to('posts#index');
+    $r->route('/posts/page/:curr')->to('posts#index');
     $r->route( '/posts/:post_id' )->to('posts#post');
 }
 
 sub index {
     my ( $self, $c ) = @_;
+    
+    my $curr = $self->param('curr') || 1;
+    $curr = $curr =~ /^\d+$/ && $curr > 0 ? $curr : 1;
+    
+    my $page_size   = 3;
+    my $frame_size  = 3;
+    
+    my $rs = $self->model->resultset('Posts');
+    
+    my $paginator = Paginator::Lite->new({
+        curr        => $curr,
+        items       => $rs->count,
+        frame_size  => $frame_size,
+        page_size   => $page_size,
+        base_url    => '/posts/page/',
+    });
 
-    $self->stash( posts => $self->model->resultset('Posts')->last_posts );
+    $self->stash(
+        posts       => $rs->last_posts( $page_size, $curr ),
+        paginator   => $paginator,
+    );
 }
 
 sub post {
